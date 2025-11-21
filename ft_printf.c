6ce872cd4cb16c7a8251fff2591b6ce1e08b8654
69Nesta <petitromeo74@gmail.com>
2025-11-21 18:06:37 +0100

    Fix: va_args not passing bc passing a copy of ap and not an pointer so on linux it working but not on mac

--- ft_printf.c
+++ ft_printf.c
@@ -6,14 +6,14 @@
 /*   By: rpetit <rpetit@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 17:10:20 by rpetit            #+#    #+#             */
-/*   Updated: 2025/11/18 08:23:42 by rpetit           ###   ########.fr       */
+/*   Updated: 2025/11/21 18:02:24 by rpetit           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static int		ft_processing(const char *format, va_list ap);
-static t_type	ft_type_selector(const char *type, va_list ap);
+static int		ft_processing(const char *format, va_list *ap);
+static t_type	ft_type_selector(const char *type, va_list *ap);
 
 int	ft_printf(const char *format, ...)
 {
@@ -23,12 +23,12 @@ int	ft_printf(const char *format, ...)
 	if (!format)
 		return (-1);
 	va_start(ap, format);
-	printed_char_count = ft_processing(format, ap);
+	printed_char_count = ft_processing(format, &ap);
 	va_end(ap);
 	return (printed_char_count);
 }
 
-static int	ft_processing(const char *format, va_list ap)
+static int	ft_processing(const char *format, va_list *ap)
 {
 	int		i;
 	int		start_current;
@@ -55,27 +55,27 @@ static int	ft_processing(const char *format, va_list ap)
 	return (total_printed);
 }
 
-static t_type	ft_type_selector(const char *type, va_list ap)
+static t_type	ft_type_selector(const char *type, va_list *ap)
 {
 	t_type	t;
 
 	t = ft_new_t_result(1, 0);
 	if (ft_istype_c(type))
-		return (ft_type_c((char)va_arg(ap, int)));
+		return (ft_type_c((char)va_arg(*ap, int)));
 	else if (ft_istype_s(type))
-		return (ft_type_s(va_arg(ap, char *)));
+		return (ft_type_s(va_arg(*ap, char *)));
 	else if (ft_istype_p(type))
-		return (ft_type_p(va_arg(ap, unsigned long)));
+		return (ft_type_p(va_arg(*ap, unsigned long)));
 	else if (ft_istype_i(type))
-		return (ft_type_i(va_arg(ap, int)));
+		return (ft_type_i(va_arg(*ap, int)));
 	else if (ft_istype_i(type))
-		return (ft_type_i(va_arg(ap, int)));
+		return (ft_type_i(va_arg(*ap, int)));
 	else if (ft_istype_u(type))
-		return (ft_type_u(va_arg(ap, unsigned int)));
+		return (ft_type_u(va_arg(*ap, unsigned int)));
 	else if (ft_istype_x(type))
-		return (ft_type_x(va_arg(ap, unsigned int), type));
+		return (ft_type_x(va_arg(*ap, unsigned int), type));
 	else if (ft_istype_xx(type))
-		return (ft_type_xx(va_arg(ap, unsigned int), type));
+		return (ft_type_xx(va_arg(*ap, unsigned int), type));
 	else if (ft_istype_mod(type))
 		return (ft_type_mod(type));
 	return (t);
